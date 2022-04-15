e73db9190467d68249497177900dc6dd86e1df9b
18gallons <ek@j2solutions.net>
2022-04-14 20:12:32 -0700

    fixed blackberry

--- ui.js
+++ ui.js
@@ -5551,7 +5551,7 @@ var themes = {      // console colour themes
         "name": "Blackberry",
         "author": "18gallons",
         "text colour": "#7B1E57",
-        "back colour": " #221C35",
+        "back colour": "#221C35",
         "accy colour": "#6b6b7f"
     },
 "git": {
