d245e5e0fbb36bc83471bd21aa967f9feb84c8f6
caffy <81343306+18gallons@users.noreply.github.com>
2025-11-06 22:35:04 -0800

    Update run.js

--- run.js
+++ run.js
@@ -2363,7 +2363,10 @@ function parseCommand(command) {
             gol_start();
         } else if (command == "loss") {
             displayAnim(loss, 10);
+        } else if (command == "早上好中国 现在我有冰淇淋 我很喜欢冰淇淋 但是 速度与激情9 比冰淇淋 速度与激情 速度与激情9 我最喜欢 所以…现在是音乐时间 准备 1 2 3 两个礼拜以后 速度与激情9 ×3 不要忘记 不要错过 记得去电影院看速度与激情9 因为非常好电影 动作非常好 差不多一样冰淇淋 再见") {
+            displayAnim("\nbing chilling");
         }
+
 
 
         else {      // new commands up here ^
